36102b584114543e9e80ddc39de8ae7912d3bdd7
andres-na-dev-admin <manazutec@gmail.com>
2024-09-26 10:28:12 -0600

    AndresNa: Filter Table

--- trello-clone/src/app/pages/table/table.component.ts
+++ trello-clone/src/app/pages/table/table.component.ts
@@ -4,18 +4,26 @@ import {CdkTableModule} from '@angular/cdk/table'
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {Product} from '../../models/product.model'
+import {DataSourceProduct} from './data-source'
+import { BtnComponent } from "../../components/btn/btn.component";
+import { debounceTime } from 'rxjs';
+import {ReactiveFormsModule} from '@angular/forms'
+import {FormControl} from '@angular/forms'
+
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule, CdkTableModule, NavbarComponent, HttpClientModule],
+  imports: [CommonModule, CdkTableModule, NavbarComponent, HttpClientModule, BtnComponent, ReactiveFormsModule],
   templateUrl: './table.component.html'
 })
 export class TableComponent {
 
-  products: Product[] = [];
-  columns: string[] = ['id', 'title', 'price', 'cover'];
-  total: number = 0;
+  //products: Product[] = [];
+  dataSource = new DataSourceProduct();
+  columns: string[] = ['#No', 'Name', 'price', 'cover', 'actions'];
+  total = 0;
+  input = new FormControl('', { nonNullable: true });
 
   constructor(
     private http: HttpClient
@@ -24,10 +32,21 @@ export class TableComponent {
   ngOnInit(): void {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
     .subscribe(data => {
-      this.products = data;
-      this.total = this.products
-        .map(item => item.price)
-        .reduce((price, total) => price + total, 0);
+      this.dataSource.init(data);
+      this.total = this.dataSource.getTotal();
     })
+
+    this.input.valueChanges
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.dataSource.find(value);
+    });
   }
+
+  update(product: Product) {
+    this.dataSource.update(product.id, { price: 20 });
+  }
+
 }
